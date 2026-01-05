9acf4443154ce2297d9f8469f48c8e7dc92c953f
Pumpkin <wishsmp1002@gmail.com>
2026-01-05 16:53:26 +1100

    Added Mace Disable, Removing debuff potions and Strength 2 and Speed 2

--- src/main/java/net/saturn/utillimiter/Utillimiter.java
+++ src/main/java/net/saturn/utillimiter/Utillimiter.java
@@ -1,11 +1,12 @@
 package net.saturn.utillimiter;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class Utillimiter implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
+        // Register the nickname command
     }
 }
